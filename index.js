5889bd6c1cabf01d6171a12ac1280c1a3f18c999
richard gichuki <chadgichuki@gmail.com>
2022-06-01 16:08:48 +0300

    Defined all functions in index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+const messages = [];
+
+function writeCards(array, event) {
+    for (let i=0; i<array.length; i++) {
+        messages.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+function countDown(int) {
+    let i = int;
+    while(i>=0) {
+        console.log(i);
+        i--;
+    }
+}
